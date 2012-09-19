2731b2c2b008ef109fe11c50793fd64b18a30fda
Matteo Depalo <matteodepalo@gmail.com>
2012-09-19 15:25:19 +0200

    Add before and after each to setup and teardown tests

--- spec/acceptance/app_creation_spec.rb
+++ spec/acceptance/app_creation_spec.rb
@@ -2,23 +2,23 @@ require 'spec_helper'
 
 describe 'App creation' do
   context 'bootstar new' do
-    it 'should create the default directory structure' do
+    before(:each) do
       system('bootstar new test-app')
+    end
 
+    after(:each) do
+      system('rm -rf test-app')
+    end
+
+    it 'should create the default directory structure' do
       File.directory?('./test-app').should be_true
       File.directory?('./test-app/test-app-api').should be_true
       File.directory?('./test-app/test-app-client').should be_true
       File.directory?('./test-app/test-app-ruby').should be_true
-
-      system('rm -rf test-app')
     end
 
     it 'should create the default api service' do
-      system('bootstar new test-app')
-
       File.file?('./test-app/test-app-api/config.ru').should be_true
-
-      system('rm -rf test-app')
     end
   end
 end
